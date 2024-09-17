9a439269fe8ce68351fc37f4c18673be0b6152a6
Jan Mollet <jan10mollet@gmail.com>
2024-09-17 13:07:10 -0400

    Definitive

--- commerce/auctions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Category, Listing, Watch
-
-admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Listing)
-admin.site.register(Watch)
-
-
